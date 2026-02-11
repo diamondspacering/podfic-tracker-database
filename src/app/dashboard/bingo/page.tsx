2a25d69bfbfccd696b56eab395816cf6fdf7ec24
d <204385202+diamondspacering@users.noreply.github.com>
2026-02-10 17:03:58 -0700

    Bingo page suspense

--- src/app/dashboard/bingo/page.tsx
+++ src/app/dashboard/bingo/page.tsx
@@ -2,6 +2,7 @@ import { Typography } from '@mui/material';
 import { Metadata } from 'next';
 import styles from '@/app/dashboard/dashboard.module.css';
 import BingoPage from './BingoPage';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Bingos',
@@ -11,7 +12,9 @@ export default function Page() {
   return (
     <div className={`${styles.body} ${styles.flexColumn}`}>
       <Typography variant='h4'>Bingos</Typography>
-      <BingoPage />
+      <Suspense>
+        <BingoPage />
+      </Suspense>
     </div>
   );
 }
